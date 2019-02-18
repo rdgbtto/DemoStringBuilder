4af381a2cfeed02e628e73e1dbdfaa3e4ccf031c
Rodrigo Almeida Bonotto <rdgbtto@gmail.com>
2019-02-17 21:56:49 -0300

    Projeto finalizado

---
+++ DemoStringBuilder/Entities/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoStringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Likes);
+            stringBuilder.Append(" Likes - ");
+            stringBuilder.AppendLine(Moment.ToString("dd/mm/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine(Content);
+            stringBuilder.AppendLine("Comments:");
+            foreach (Comment comment in Comments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
